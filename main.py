1d243ee1f777f33e34caee60ca4fa11a1f125e5f
Gloid <peter.niklasson@outlook.com>
2023-07-12 21:47:53 +0200

    Problem med att spara filtrerad ndarray till bild

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import scipy.misc as sm
-import imageio as iio
+import imageio.v2 as iio
 import numpy as np
 import matplotlib.pyplot as plt
-im = iio.imread('kvinna.jpg')[:,:,0]
-iio.imsave('new_kvinna.jpg',im)
-print(np.shape(im))
+from PIL import Image
+image = Image.open('kvinna.jpg').convert('L')
+
 
 def Wavelet(n):
     #Creates and returns a Wavelet matrix (n*n)
@@ -15,4 +15,17 @@ def Wavelet(n):
         W[i,i*2+1]=1/2
         W[i+int(n/2),i*2]=-1/2
         W[i+int(n/2),i*2+1]=1/2
-    return np.sqrt(2)*W
+    return W
+
+def compression1d(image):
+    Wm = Wavelet(np.shape(image)[0])
+    return np.abs(np.rint(np.matmul(Wm,image))).astype('int')
+
+
+def compression2d(image):
+    Wm = Wavelet(np.shape(image)[0])
+    Wn = Wavelet(np.shape(image)[1])
+    return np.rint(np.matmul(np.matmul(Wm,im),Wn.transpose())).astype('int')
+
+compressedImage = Image.fromarray(compression1d(image))
+compressedImage.save('filtrerad_kvinna.jpg')
