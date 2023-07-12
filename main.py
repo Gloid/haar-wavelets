222f1302aca37df98066674187c8414c954e3492
Gloid <32645855+Gloid@users.noreply.github.com>
2023-07-12 19:26:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-
+import scipy.misc as sm
+import imageio.v2 as iio
+import numpy as np
+import matplotlib.pyplot as plt
+im = iio.imread('kvinna.jpg')[:,:,0]
+iio.imsave('new_kvinna.jpg',im)
+print(np.shape(im))
