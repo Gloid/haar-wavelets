66d87356c1f8eabccdc34d7847ff0ad4a79d24f6
Gloid <peter.niklasson@outlook.com>
2023-07-13 00:35:38 +0200

    Creating plot of compressed image

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import scipy.misc as sm
 import imageio.v2 as iio
 import numpy as np
 import matplotlib.pyplot as plt
-from PIL import Image
-image = Image.open('kvinna.jpg').convert('L')
+from PIL import Image, ImageOps
+testIm = np.asarray(Image.open('kvinna.jpg').convert('L'))
 
 
 def Wavelet(n):
@@ -15,7 +15,7 @@ def Wavelet(n):
         W[i,i*2+1]=1/2
         W[i+int(n/2),i*2]=-1/2
         W[i+int(n/2),i*2+1]=1/2
-    return W
+    return np.sqrt(2)*W
 
 def compression1d(image):
     Wm = Wavelet(np.shape(image)[0])
@@ -25,7 +25,14 @@ def compression1d(image):
 def compression2d(image):
     Wm = Wavelet(np.shape(image)[0])
     Wn = Wavelet(np.shape(image)[1])
-    return np.rint(np.matmul(np.matmul(Wm,im),Wn.transpose())).astype('int')
+    return np.abs(np.rint(np.matmul(np.matmul(Wm,image),Wn.transpose()))).astype('int')
 
-compressedImage = Image.fromarray(compression1d(image))
+plt.imshow(compression2d(testIm), cmap='gray',interpolation='nearest')
+plt.savefig('filtrerad_kvinna.jpg',bbox_inches='tight')
+plt.show()
+
+#Dont know why ImageOps.grayscale is necessary, get error if removed
+#Saved image has poor quality compared to the plotted image
+compressedImage = ImageOps.grayscale(Image.fromarray(compression2d(testIm)))
+
 compressedImage.save('filtrerad_kvinna.jpg')
