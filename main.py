6bc4a4c9c14e98001daf07e00f3d2ff1fa2e81fa
Gloid <peter.niklasson@outlook.com>
2023-07-12 20:09:12 +0200

    Function to create wavelet matrixes

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ import matplotlib.pyplot as plt
 im = iio.imread('kvinna.jpg')[:,:,0]
 iio.imsave('new_kvinna.jpg',im)
 print(np.shape(im))
+
+def Wavelet(n):
+    #Creates and returns a Wavelet matrix (n*n)
+    #n (even integer) dimension of matrix
+    W = np.zeros([n,n])
+    for i in range(int(n/2)):
+        W[i,i*2]=1/2
+        W[i,i*2+1]=1/2
+        W[i+int(n/2),i*2]=-1/2
+        W[i+int(n/2),i*2+1]=1/2
+    return W
+print(Wavelet(10))
