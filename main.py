b8dee02c4317a3f92db855142ec4f5c06adaf813
Gloid <peter.niklasson@outlook.com>
2023-07-20 15:12:03 +0200

    Added methods

--- main.py
+++ main.py
@@ -20,9 +20,11 @@ def Wavelet(n):
         W[i+int(n/2),i*2+1]=1/2
     return np.sqrt(2)*W
 
-def compression1d(image):
+def HWT1D(image):
     #Returns ndarray of the 1-dimensional HWT of image
     #image (ndarray) 
+    if np.shape(image)[0] % 2 != 0:
+        np.delete(image,(np.shape(image)[0]-1),axis=0)
     Wm = Wavelet(np.shape(image)[0])
     newImage = np.matmul(Wm,image)
     newImage  = np.abs(np.rint(newImage))
@@ -30,9 +32,13 @@ def compression1d(image):
     return newImage
 
 
-def compression2d(image):
+def HWT2D(image):
     #Returns ndarray of the 2-dimensional HWT of image
     #image (ndarray) 
+    if np.shape(image)[0] % 2 != 0:
+        np.delete(image,(np.shape(image)[0]-1),axis=0)
+    if np.shape(image)[1] % 2 != 0:
+        np.delete(image, (np.shape(image)[1]-1),axis=1)
     Wm = Wavelet(np.shape(image)[0])
     Wn = Wavelet(np.shape(image)[1])
     newImage = np.matmul(np.matmul(Wm,image),Wn.transpose())
@@ -41,11 +47,36 @@ def compression2d(image):
     newImage *= (255.0/newImage.max())
     return newImage
 
-plt.imshow(compression2d(testIm), cmap='gray',interpolation='nearest')
-#plt.savefig('filtrerad_kvinna.jpg',bbox_inches='tight')
-plt.show()
+def inverseHaarTransformation(im1,im2,im3,im4):
+    newImage = np.concatenate(np.concatenate(im1,im2,axis=0), np.concatenate(im3,im4,axis=0),axis=1)
+    Wm = Wavelet(np.shape(newImage)[0])
+    Wn = Wavelet(np.shape(newImage)[1])    
+    newImage = np.matmul(np.matmul(Wm.transpose(),newImage),Wn)
+    newImage  = np.abs(np.rint(newImage))
+    newImage *= (255.0/newImage.max())
 
-compressedImage = Image.fromarray(np.uint8((compression2d(testIm))), 'L')
+    return newImage
+
+def inverseHaarTransformation(newImage):
+    Wm = Wavelet(np.shape(newImage)[0])
+    Wn = Wavelet(np.shape(newImage)[1])    
+    newImage = np.matmul(np.matmul(Wm.transpose(),newImage),Wn)
+    newImage  = np.abs(np.rint(newImage))
+    newImage *= (255.0/newImage.max())
+
+    return newImage
+
+def Haarcompression(image, n):
+    for i in range(n):
+        image = HWT2D(image)
+        image = image[0:int(np.shape(image)[0]/2),0:int(np.shape(image)[1]/2)]
+    return image
+
+
+compressedImage = Image.fromarray(np.uint8((Haarcompression(testIm,3))), 'L')
 
 compressedImage.show()
+
+#originalImage = Image.fromarray(np.uint8((inverseHaarTransformation(HWT2D(testIm)))), 'L')
+#originalImage.show()
 #compressedImage.save('komprimerad_kvinna.jpg')
