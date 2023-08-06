35307217ab3064078009d403f254a180af5b2ba1
Gloid <peter.niklasson@outlook.com>
2023-08-06 20:25:28 +0200

    Update presentation.py

--- presentation.py
+++ presentation.py
@@ -1,1 +1,21 @@
 
+'''
+Inledning/Teori - Jacob
+Hur man skapar matrisen - Jacob
+'''
+''' 
+1-dimensionell transformation + 2-dimensionell transform - Peter
+'''
+
+'''
+Invers - Elias
+'''
+'''
+Kompression/iteration
+'''
+'''
+Manuella medelvärden - Maximilian
+'''
+'''
+Exempel/Demonstration
+'''
