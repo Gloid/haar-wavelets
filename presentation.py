d957ea1533907e2b0a2c4f62b3ee1a75f29d5519
JacobJannering <128799755+JacobJannering@users.noreply.github.com>
2023-08-06 20:32:24 +0200

    Update presentation.py

--- presentation.py
+++ presentation.py
@@ -1,8 +1,20 @@
 
 '''
 Inledning/Teori - Jacob
+
+
 Hur man skapar matrisen - Jacob
 '''
+def HaarWavelet(n):
+    
+    Matrix= np.zeros([n,n])
+    
+    for i in range(int(n/2)):
+        Matrix[i,i*2]=1/2
+        Matrix[i,i*2+1]=1/2
+        Matrix[i+int(n/2),i*2]=-1/2
+        Matrix[i+int(n/2),i*2+1]=1/2
+    return np.sqrt(2)*Matrix
 ''' 
 1-dimensionell transformation + 2-dimensionell transform - Peter
 '''
